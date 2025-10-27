e92daee7db3b292672ea048bc69fcb7521298187
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-10-27 11:26:11 +0800

    Add files via upload

---
+++ triral_eror.py
@@ -0,0 +1,17 @@
+months = ["jan","feb","mar","apr","may","jun","jul"]
+months.append("aug")
+print(months)
+months.pop()
+print(months)
+months.remove("mar")
+print(months)
+months.insert(4,"new")
+print(months)
+#iteration
+# for m in months:
+#     print(f"{m},2025")
+
+fullname = 'mark juben pedernal'
+
+# #list slicing
+print(fullname[::-1])
