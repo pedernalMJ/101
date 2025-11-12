4869edc56b866f54d98febb7e08802f17f26ec82
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-11-12 11:27:17 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,28 @@
+# from activity25_2 import diamond
+# name = input("Hi, What is your name")
+# (f"Welcome{name}, to my compiler program")
+
+# iscontinue=True
+
+# while iscontinue=True:
+#   print("select a program")
+#   print("A - activity 1\nB actvivty 2\nC activity15\nD factorial\n E-exit")
+
+# choose = input("What program/code you would like to run").lower()
+
+# if choose == "a":
+#   activity1()
+#   continue
+# elif choose == "b":
+#   activity2()
+#   continue
+# elif choose == "c":
+#   activity15()
+#   continue
+# elif choose == "d":
+#   activity15()
+#   continue
+# else:
+#   choose == "e"
+  
+
